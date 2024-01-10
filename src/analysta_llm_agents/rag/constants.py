2cd683f53fb44f31095d925a992e21aa60d87053
arozumenko <artyom.rozumenko@gmail.com>
2024-01-10 17:10:34 +0200

    rag qna agent

---
+++ src/analysta_llm_agents/rag/constants.py
@@ -0,0 +1,10 @@
+guidance_message = """Text from documents for analysis follows:"""
+
+context_message = """You are bot that provide unswers based on data from the various document. 
+Your task is to provide accurate answer to the question."""
+
+weights = {
+    'keywords': 0.2,
+    'document_summary': 0.5,
+    'data': 0.3
+}
